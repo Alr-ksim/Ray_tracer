426a8858c2315f8cc74c8778552b86542576d956
廖伟鑫 <liaoweixin@sjtu.edu.cn>
2021-07-19 09:18:55 +0800

    fmt

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -1,7 +1,7 @@
-use crate::vec3::Vec3;
-use crate::vec3;
 use crate::ray::Ray;
 use crate::tools;
+use crate::vec3;
+use crate::vec3::Vec3;
 
 #[derive(Debug)]
 pub struct Camera {
@@ -16,21 +16,30 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f64, aspect_ratio: f64, aperture: f64, focus_dist: f64) -> Self {
-        let theta:f64 = tools::dtr(vfov);
-        let h:f64 = (theta/2.0).tan();
-        let viewport_height:f64 = 2.0 * h;
-        let viewport_width:f64 = aspect_ratio * viewport_height;
-        
-        let w:Vec3 = (lookfrom.clone() - lookat.clone()).unit();
-        let u:Vec3 = (Vec3::cross(vup.clone(), w.clone())).unit();
-        let v:Vec3 = Vec3::cross(w.clone(), u.clone());
+    pub fn new(
+        lookfrom: Vec3,
+        lookat: Vec3,
+        vup: Vec3,
+        vfov: f64,
+        aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Self {
+        let theta: f64 = tools::dtr(vfov);
+        let h: f64 = (theta / 2.0).tan();
+        let viewport_height: f64 = 2.0 * h;
+        let viewport_width: f64 = aspect_ratio * viewport_height;
 
-        let ori:Vec3 = lookfrom.clone();
-        let hor:Vec3 = u.clone() * focus_dist * viewport_width;
-        let ver:Vec3 = v.clone() * focus_dist * viewport_height;
-        let low:Vec3 = ori.clone() - hor.clone()/2.0 - ver.clone()/2.0 - w.clone() * focus_dist;
-        let len:f64 = aperture / 2.0;
+        let w: Vec3 = (lookfrom.clone() - lookat.clone()).unit();
+        let u: Vec3 = (Vec3::cross(vup.clone(), w.clone())).unit();
+        let v: Vec3 = Vec3::cross(w.clone(), u.clone());
+
+        let ori: Vec3 = lookfrom.clone();
+        let hor: Vec3 = u.clone() * focus_dist * viewport_width;
+        let ver: Vec3 = v.clone() * focus_dist * viewport_height;
+        let low: Vec3 =
+            ori.clone() - hor.clone() / 2.0 - ver.clone() / 2.0 - w.clone() * focus_dist;
+        let len: f64 = aperture / 2.0;
 
         Self {
             origin: ori.clone(),
@@ -43,10 +52,17 @@ impl Camera {
             lens_radius: len.clone(),
         }
     }
-    
+
     pub fn get_ray(&self, s: f64, t: f64) -> Ray {
-        let rd:Vec3 = vec3::rand_in_unit_disk()*self.lens_radius;
-        let offset:Vec3 = self.cu.clone()*rd.x() + self.cv.clone()*rd.y();
-        Ray::new(self.origin.clone() + offset.clone(), self.lower_left_corner.clone() + self.horizontal.clone()*s + self.vertical.clone()*t - self.origin.clone() - offset.clone())
+        let rd: Vec3 = vec3::rand_in_unit_disk() * self.lens_radius;
+        let offset: Vec3 = self.cu.clone() * rd.x() + self.cv.clone() * rd.y();
+        Ray::new(
+            self.origin.clone() + offset.clone(),
+            self.lower_left_corner.clone()
+                + self.horizontal.clone() * s
+                + self.vertical.clone() * t
+                - self.origin.clone()
+                - offset.clone(),
+        )
     }
 }
--- raytracer/src/tools.rs
+++ raytracer/src/tools.rs
@@ -2,8 +2,8 @@ extern crate rand;
 
 use rand::Rng;
 
-pub const INF:f64 = 9223372036854775807.0;
-pub const PI:f64 = 3.1415926535897932385;
+pub const INF: f64 = 9223372036854775807.0;
+pub const PI: f64 = 3.1415926535897932385;
 
 pub fn dtr(degree: f64) -> f64 {
     degree * PI / 180.0
@@ -15,7 +15,11 @@ pub fn randf(low: f64, high: f64) -> f64 {
 }
 
 pub fn clamp(x: f64, x_min: f64, x_max: f64) -> f64 {
-    if x < x_min { return x_min; }
-    if x > x_max { return x_max; }
+    if x < x_min {
+        return x_min;
+    }
+    if x > x_max {
+        return x_max;
+    }
     return x;
 }
